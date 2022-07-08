d713c84951ca1fa72ee808b15962be6ea709847b
Shiva TEClab <shivajreddy@outlook.com>
2022-07-08 15:05:42 -0400

    useReducer finished all extra credits

--- advanced-react-hooks/src/exercise/01.js
+++ advanced-react-hooks/src/exercise/01.js
@@ -4,23 +4,42 @@
 import * as React from 'react'
 import ReducerComp from './ReducerComp'
 
-function reducer(state, {step}) {
-  return state + step
+const ACTIONS = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement',
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.INCREMENT:
+      return {count: state.count + 1}
+    case ACTIONS.DECREMENT:
+      return {count: state.count - 1}
+    default:
+      return new Error(`Action type not found ${action.type}`)
+  }
 }
 
 function Counter({initialCount = 0, step = 10}) {
   // const [count, setCount] = React.useReducer(reducer, initialCount)
-  const [count, changeCount] = React.useReducer(reducer, initialCount)
+  // const [count, changeCount] = React.useReducer(reducer, {count: initialCount})
+  const [state, changeCount] = React.useReducer(reducer, {count: initialCount})
 
-  const increment = () => changeCount({step})
-  return <button onClick={increment}>{count}</button>
+  // const increment = () => changeCount({step: step})
+  return (
+    <>
+      <button onClick={() => changeCount({type: ACTIONS.DECREMENT})}>-</button>
+      <span>{state.count}</span>
+      <button onClick={() => changeCount({type: ACTIONS.INCREMENT})}>+</button>
+    </>
+  )
 }
 
 function App() {
   return (
     <>
-      {/* <Counter /> */}
-      <ReducerComp />
+      <Counter />
+      {/* <ReducerComp /> */}
     </>
   )
 }
